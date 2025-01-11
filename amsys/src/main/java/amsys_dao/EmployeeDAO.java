ac8f83b4fc1a4dbc5c28d3a1dff12888a6d90f3c
akki_0402 <70006096+amdegi0402@users.noreply.github.com>
2025-01-11 13:26:15 +0900

    Update EmployeeDAO.java

--- amsys/src/main/java/amsys_dao/EmployeeDAO.java
+++ amsys/src/main/java/amsys_dao/EmployeeDAO.java
@@ -14,11 +14,7 @@ import java.sql.SQLException;
 
 import amsys_entity.Employee;
 
-/**
- *
- * @author FLM
- * @version 1.0.0
- */
+
 public class EmployeeDAO implements Serializable{
 	private Connection con;
 
